daa4652e5a3c163838c8c79fa25a1e203159ca89
masterkn48 <masterkn48@gmail.com>
2020-05-10 17:14:11 +0530

    fix

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,36 +1,35 @@
 import React from "react";
-import { Route,Switch } from "react-router-dom";
-import Home from './core/Home';
-import Navbar from './core/NavBar';
-import Signup from './user/Signup';
-import Profile from './user/Profile';
-import Login from './user/Login';
-import User from './user/User';
-import EditProfile from './user/EditProfile';
-import Findpeople from './user/Findpeople';
-import Post from './post/Post';
-import PrivateRoute from  './auth/PrivateRoute';
-import SinglePost from './post/SinglePost';
-import EditPost from './post/EditPost';
+import { Route, Switch } from "react-router-dom";
+import Home from "./core/Home";
+import Navbar from "./core/NavBar";
+import Signup from "./user/Signup";
+import Profile from "./user/Profile";
+import Login from "./user/Login";
+import User from "./user/User";
+import EditProfile from "./user/EditProfile";
+import Findpeople from "./user/Findpeople";
+import Post from "./post/Post";
+import PrivateRoute from "./auth/PrivateRoute";
+import SinglePost from "./post/SinglePost";
+import EditPost from "./post/EditPost";
 import ForgotPassword from "./user/ForgetPassword";
 import ResetPassword from "./user/ResetPassword";
-import Admin from './admin/Admin'
-import StickyFooter from 'react-sticky-footer';
+import Admin from "./admin/Admin";
+import StickyFooter from "react-sticky-footer";
 
 const NoMatchPage = () => {
   return (
     <div id="main">
-    	<div className="fof">
-        		<h1>Error 404</h1>
-    	</div>
-</div>
+      <div className="fof">
+        <h1>Error 404</h1>
+      </div>
+    </div>
   );
 };
 const Router = () => (
-  
   // eslint-disable-next-line no-unused-expressions
   <div>
-    <Navbar/>
+    <Navbar />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/signup" exact component={Signup} />
@@ -44,24 +43,30 @@ const Router = () => (
       <Route path="/login" exact component={Login} />
       <Route path="/users" exact component={User} />
       <Route exact path="/forgot-password" component={ForgotPassword} />
-      <Route exact path="/reset-password/:resetPasswordToken" component={ResetPassword} />
+      <Route
+        exact
+        path="/reset-password/:resetPasswordToken"
+        component={ResetPassword}
+      />
       <Route component={NoMatchPage} />
     </Switch>
-    <StickyFooter className='jumbrotron'
-    bottomThreshold={50}
-    normalStyles={{
-    backgroundColor: "#0E2F56",
-    padding: "2rem",
-    color:'white',
-    textAlign:'center'
-    }}
-    stickyStyles={{
-    backgroundColor: "rgba(14, 47, 86,.8)",
-    padding: "2rem"
-    }}
->
-    ReadWrite 2019
-</StickyFooter>
+    <StickyFooter
+      className="jumbrotron"
+      bottomThreshold={50}
+      normalStyles={{
+        backgroundColor: "#0E2F56",
+        padding: "2rem",
+        color: "white",
+        textAlign: "center",
+      }}
+      stickyStyles={{
+        backgroundColor: "rgba(14, 47, 86,.8)",
+        padding: "2rem",
+      }}
+    >
+      ReadWrite 2020 <br />
+      This is dummy project
+    </StickyFooter>
   </div>
 );
 
